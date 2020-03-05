dc0f72df79c6702894d6c44ad3f980091d0f3cb1
samburgess <saburgess239@gmail.com>
2020-03-05 10:41:17 -0800

    Updates

---
+++ precache-manifest.e587b155552109ee1e2df36b9b3c11c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec91567959c030abd9a5127cd0b1b1b9",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "3ceeb553ec76aa7c2181",
+    "url": "/testapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7fb2618bb8bd4d2f033",
+    "url": "/testapp/static/js/2.2cff6414.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/testapp/static/js/2.2cff6414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ceeb553ec76aa7c2181",
+    "url": "/testapp/static/js/main.e732fe69.chunk.js"
+  },
+  {
+    "revision": "de5708c49bd6d043bd26",
+    "url": "/testapp/static/js/runtime-main.35930876.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
